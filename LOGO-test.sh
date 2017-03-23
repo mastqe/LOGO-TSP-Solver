0d45cf3ee68b6f714eb02b38922a741eda588e8b
Zamua Nasrawt <nasrawzo@dukes.jmu.edu>
2017-03-23 18:33:58 -0400

    Fix formatting of script output

--- LOGO-test.sh
+++ LOGO-test.sh
@@ -12,18 +12,20 @@ fi
 for file in $(cat tsps); do
     printf "file: $file\n"
 
+    let num=$(echo $file | sed "s/[a-z]*//g")
+
     # iterate over desired core counts
     for i in 1 2 4 8 16 32 64; do
         printf "cores: $i\n"
 
-        ./logo --in=../tsplib/$file --mode=cpupar --err=0.05f      \
-               --showLOInfo=1       --autoDevice  --maxCoresCPU=$i \
-               > ./results/${file%.tsp}_${i}_cpu
+        ./logo --in=../tsplib/${file}.tsp --mode=cpupar --err=0.05f      \
+               --showLOInfo=1             --autoDevice  --maxCoresCPU=$i \
+               > ./results/${num}_${i}_cpu
     done
 
     # execute on GPU
     printf "GPU\n"
-    ./logo --in=../tsplib/$file --mode=cuda --err=0.05f \
-           --showLOInfo=1       --autoDevice            \
-           > ./results/${file%.tsp}_gpu
+    ./logo --in=../tsplib/${file}.tsp --mode=cuda --err=0.05f \
+           --showLOInfo=1             --autoDevice            \
+           > ./results/${num}_gpu
 done
--- results.sh
+++ results.sh
@@ -3,25 +3,33 @@
 # get final found length of path
 function length
 {
-    printf "%s" $(cat $1 | grep "New global minimum" | tail -n1 | cut -d">" -f2 | cut -d" " -f3)
+    printf "%s" $(cat $1 | \
+                  grep "New global minimum" | \
+                  tail -n1 | \
+                  cut -d">" -f2 | \
+                  cut -d" " -f3)
 }
 
 # get time
 function path_time
 {
-    printf "%s" $(tail -n1 $1 | cut -d" " -f 3)
+    printf "%s" $(tail -n1 $1 | \
+                  cut -d" " -f 3)
 }
 
 # get accuracy
 function acc
 {
-    printf "%s" $(cat $1 | grep "New global minimum" | tail -n1 | cut -d">" -f2 | cut -d" " -f5)
+    printf "%s" $(cat $1 | \
+                  grep "New global minimum" | \
+                  tail -n1 | \
+                  cut -d">" -f2 | \
+                  cut -d" " -f5)
 }
 
 for f in results/*; do
     printf "%s,"  ${f:8}
     printf "%s,"  $(length $f)
-    printf "%s,"  $(acc $f)
+    printf "%s,"  $(acc $f) | sed "s/%//"
     printf "%s\n" $(path_time $f)
 done
-
