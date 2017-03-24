fbac102830947b900cb829b6bdd6d41ee5c9369d
Quincy Mast <mastqe@dukes.jmu.edu>
2017-03-24 13:37:39 -0400

    let script run trials

--- LOGO-test.sh
+++ LOGO-test.sh
@@ -5,27 +5,37 @@ if [ -e "out.txt" ]; then
 fi
 
 if [ ! -d "results" ]; then
-    mkdir results   
+    mkdir results
 fi
 
-# iterate over files lists in "tsps"
-for file in $(cat tsps); do
-    printf "file: $file\n"
+arg=${1:-1}
 
-    let num=$(echo $file | sed "s/[a-zA-Z]*//g")
+for x in $(seq 1 $arg); do
+    # iterate over files lists in "tsps"
+    for file in $(cat tsps); do
+        printf "file: $file\n"
 
-    # execute on GPU
-    printf "GPU\n"
-    ./logo --in=../tsplib/${file}.tsp --mode=cuda --err=0.05f \
-           --showLOInfo=1             --autoDevice            \
-           > ./results/${num}_gpu
-    
-    # iterate over desired core counts
-    for i in 64 32 16 8 4 2 1; do
-        printf "cores: $i\n"
+        let num=$(echo $file | sed "s/[a-zA-Z]*//g")
 
-        ./logo --in=../tsplib/${file}.tsp --mode=cpupar --err=0.05f      \
-               --showLOInfo=1             --autoDevice  --maxCoresCPU=$i \
-               > ./results/${num}_${i}_cpu
+        # execute on GPU
+        printf "GPU\n"
+        ./logo --in=../tsplib/${file}.tsp --mode=cuda --err=0.05f \
+               --showLOInfo=1             --autoDevice            \
+               > ./results/${num}_gpu
+
+        # iterate over desired core counts
+        for i in 64 32 16 8 4 2 1; do
+            printf "cores: $i\n"
+
+            ./logo --in=../tsplib/${file}.tsp --mode=cpupar --err=0.05f      \
+                   --showLOInfo=1             --autoDevice  --maxCoresCPU=$i \
+                   > ./results/${num}_${i}_cpu
+        done
     done
+
+    printf "\n ----- Saving $x -----\n\n"
+    ./results.sh
+    mv data.csv ${x}.csv
+    rm -r results
+    mkdir results
 done
